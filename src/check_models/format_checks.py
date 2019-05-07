274d53a523f599c7b14911afaac2df7686ff0ba4
martamatos <marta.ra.matos@gmail.com>
2019-05-07 13:38:04 +0200

    Added code to check models

---
+++ src/check_models/format_checks.py
@@ -0,0 +1,84 @@
+import numpy as np
+
+
+def check_met_rxn_order(data_dict: dict) -> bool:
+    """
+    Given the excel file as argument, checks if the order of reactions and metabolites in all sheets is consistent
+    with the order in the stoichiometry matrix.
+    If everything is fine flag is 0, otherwise it is set to 1.
+
+    :param data_dict: a dictionary that represents the excel file with the GRASP model.
+    :return: flag
+    """
+
+    print('\nChecking if the order of reactions and metabolites is the same in all excel sheets.\n')
+
+    flag = False
+    rxn_list = data_dict['stoic']['rxn ID'].values
+    met_list = data_dict['stoic'].columns.values[1:]
+
+    for key in list(data_dict.keys())[2:]:
+        id_list = data_dict[key].iloc[:, 0].values
+
+        if key != 'measRates':
+            met_bool = id_list == met_list
+            met_bool = all(met_bool) if isinstance(met_bool, np.ndarray) else met_bool
+
+            rxn_bool = id_list == rxn_list
+            rxn_bool = all(rxn_bool) if isinstance(rxn_bool, np.ndarray) else rxn_bool
+
+            if not met_bool and not rxn_bool:
+                print('Metabolite or reaction list in sheet ', key,
+                      'doesn\'t match the list in the stoichiometry matrix.')
+                print('Current list:\n', id_list)
+                print('Metabolite list in stoichiometry matrix:\n', met_list)
+                print('Reaction list in stoichiometry matrix:\n', rxn_list)
+                print('\n')
+                flag = True
+
+    return flag
+
+
+def _check_kinetics_column(data_dict: dict, col_name: str) -> bool:
+
+    flag = False
+    col_data = data_dict[col_name].dropna()
+    for row in col_data:
+        if row.find(',') != -1 or row.find(';') != -1 or row.find('.') != -1:
+            print('Make sure all metabolites are separated by a single space in column "', col_name, '" row:\n', row)
+            flag = True
+
+    return flag
+
+
+def check_kinetics_met_separators(data_dict: dict) -> bool:
+    """
+    Given the excel file as argument, in the kinetics sheet for columns where cells can have multiple values, makes
+     sure these values are not separated by a comma, semi-colon, or dot.
+    If everything is fine flag is 0, otherwise it is set to 1.
+
+    :param data_dict: a dictionary that represents the excel file with the GRASP model.
+    :return: flag
+    """
+
+    print(
+        '\nChecking if values are separated by a space in the kinetics sheet in columns order, promiscuous, inhibitors, activators, negative effector, and positive effector.\nIt looks for dots, commas, and semi-colons.')
+
+    flag_list = []
+
+    for key in list(data_dict.keys()):
+        if key.startswith('kinetics'):
+            flag = _check_kinetics_column(data_dict[key], 'order')
+            flag_list.append(flag)
+            flag = _check_kinetics_column(data_dict[key], 'promiscuous')
+            flag_list.append(flag)
+            flag = _check_kinetics_column(data_dict[key], 'inhibitors')
+            flag_list.append(flag)
+            flag = _check_kinetics_column(data_dict[key], 'activators')
+            flag_list.append(flag)
+            flag = _check_kinetics_column(data_dict[key], 'negative effector')
+            flag_list.append(flag)
+            flag = _check_kinetics_column(data_dict[key], 'positive effector')
+            flag_list.append(flag)
+
+    return any(flag_list)
---
+++ src/check_models/mass_balance_checks.py
@@ -0,0 +1,83 @@
+
+def check_flux_balance(data_dict: dict) -> bool:
+    """
+    When all fluxes are specified in the measRates sheet, check if all metabolites are mass balanced (well, the ones
+    that are marked as balanced in the mets sheet)-
+    If everything is fine flag is 0, otherwise it is set to 1.
+
+    :param data_dict: a dictionary that represents the excel file with the GRASP model.
+    :return: flag (bool)
+    """
+
+    flag = False
+    flux_df = data_dict['measRates']
+    mets_df = data_dict['mets']
+    stoic_df = data_dict['stoic']
+
+    if len(stoic_df.index) == len(flux_df.index):
+        print('\nChecking if the fluxes for each metabolite production/consumptions add up to zero.\n')
+
+        stoic_df.index = stoic_df['rxn ID']
+        del stoic_df['rxn ID']
+
+        met_in_rxns = dict()
+        for col in stoic_df.columns:
+            rxn_list = stoic_df.loc[stoic_df[col].ne(0), col]
+            met_in_rxns[col] = rxn_list.to_dict()
+
+        mets_df.index = mets_df['ID']
+        fixed_mets = set(mets_df.loc[mets_df['fixed?'].eq(1), 'fixed?'].index.values)
+        balanced_mets = set(mets_df.loc[mets_df['balanced?'].eq(1), 'balanced?'].index.values)
+
+        flux_df.index = flux_df[flux_df.columns[0]]
+        del flux_df[flux_df.columns[0]]
+        mean_col = flux_df.columns[0]
+
+        for met in met_in_rxns.keys():
+
+            flux_balance = sum([met_in_rxns[met][key] * flux_df.loc[key, mean_col] for key in met_in_rxns[met].keys()])
+
+            if flux_balance != 0 and met in balanced_mets:
+                print('The flux for', met, 'is not balanced. The difference in flux is', flux_balance)
+                flag = True
+            elif flux_balance == 0 and met not in balanced_mets:
+                print(met, 'should be in balanced mets')
+                flag = True
+
+    return flag
+
+
+def check_balanced_metabolites(data_dict: dict) -> bool:
+    """
+    Checks if metabolites that are both consumed and produced in the stoichiometric matrix are marked as balanced and
+    the other way around. Checking for mass balances is more accurate though.
+    If everything is fine flag is 0, otherwise it is set to 1.
+
+    :param data_dict (dict): a dictionary that represents the excel file with the GRASP model.
+    :return: flag (bool)
+    """
+
+    print(
+        '\nChecking if metabolites are both consumed and produced in the stoichiometric matrix, and if so checks if they are marked as balanced in the mets sheet. However, the metabolite might be balanced/not balanced anyways depending on the flux of the reactions that consume/produce it, so take this with a grain of salt.\n ')
+
+    flag = False
+    stoic_df = data_dict['stoic']
+    stoic_df.index = stoic_df['rxn ID']
+    stoic_df = stoic_df.drop('rxn ID', axis=1)
+    mets_df = data_dict['mets']
+
+    for i, met in enumerate(stoic_df.columns):
+        if stoic_df[met].gt(0).any() and stoic_df[met].lt(0).any():
+            if mets_df['balanced?'][i] == 0:
+                print(met, 'is marked as not balanced but it seems to be balanced.')
+                flag = True
+        else:
+            if mets_df['balanced?'][i] == 1:
+                print(met, 'is marked as balanced but it does not seem to be balanced.')
+                flag = True
+            if mets_df['fixed?'][i] == 0:
+                print(met, 'is not set as constant but maybe it should, since it does not seem to be balanced.')
+                flag = True
+
+    return flag
+
