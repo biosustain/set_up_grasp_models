65349011f617b6af121570ad3cf9cc24ec6cfc33
martamatos <marta.ra.matos@gmail.com>
2019-05-08 15:45:46 +0200

    Added examples folder

---
+++ examples/check_input_model.py
@@ -0,0 +1,27 @@
+import os
+
+import pandas as pd
+from set_up_grasp_models import check_met_rxn_order, check_kinetics_met_separators, check_flux_balance, \
+    check_balanced_metabolites, check_thermodynamic_feasibility
+
+
+# import the model
+file_in = os.path.join('models', 'HMP2360_r0_t0.xlsx')
+data_dict = pd.read_excel(file_in, sheet_name=None)
+
+
+# check metabolite lists separators in kinetics sheet
+check_kinetics_met_separators(data_dict)
+
+# check if the order of metabolites and reactions in all excel sheets is correct
+check_met_rxn_order(data_dict)
+
+# check consistency between stoic sheet and mets sheet, take the results with a grain of salt
+check_balanced_metabolites(data_dict)
+
+# check if fluxes and Gibbs energies are compatible
+check_thermodynamic_feasibility(data_dict)
+
+# check if all metabolites marked as balanced are indeed mass balance, only works if fluxes for all reactions are
+# either specified or can be calculated
+check_flux_balance(data_dict)
