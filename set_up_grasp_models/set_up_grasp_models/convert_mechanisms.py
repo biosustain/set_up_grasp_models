7974b71d8e0e43bd450ba80915968a5ee7079526
martamatos <marta.ra.matos@gmail.com>
2019-05-14 16:37:59 +0200

    convert_mechanisms now working for most mechanisms in the patterns folder

--- set_up_grasp_models/set_up_grasp_models/convert_mechanisms.py
+++ set_up_grasp_models/set_up_grasp_models/convert_mechanisms.py
@@ -41,7 +41,7 @@ def _get_enz_states(er_mech: str) -> list:
     return enz_states_list
 
 
-def _generate_grasp_pattern(enz_states_list):
+def _generate_grasp_pattern(enz_states_list: list, inhib_list: list = None, activ_list: list = None) -> str:
     """
     Given a list of enzyme state numbers and metabolites that bind/unbind to them, generates the respective
     GRASP pattern.
@@ -50,7 +50,7 @@ def _generate_grasp_pattern(enz_states_list):
        - the number of reactions in a mechanism is counted. A new reaction starts when the enzyme state to which
        a ligand can bind is 1 (i.e. the free enzyme) and there has been more than one transition step. This allows
        one to distinguish different reactions in a given promiscuous enzyme mechanism and naming each first product
-       to be released after a transition step as P{i}, where stands for the number of the reaction. This is important
+       to be released after a transition step as P{i}, where i stands for the number of the reaction. This is important
        so GRASP can later on write the flux equation correctly for each reaction catalyzed by a promiscuous enzyme.
        The reason reactions are counted and not only transition steps is because ping-pong mechanisms also have
        multiple transition steps and yet the first product to be released after a transition step should be named
@@ -59,8 +59,13 @@ def _generate_grasp_pattern(enz_states_list):
        - whether we are in a state right after the transition step or not is tracked, so that we can name the product
        as P{i} instead of Q, R, S, etc.
 
+    Inhibiting and activating metabolites are assumed to only bind to the enzyme and not be released, except as
+    a "reverse binding".
+
     Args:
         enz_states_list (list): list of all enzyme state numbers and ligands that bind/unbind.
+        inhib_list (list): list of inhibiting metabolites.
+        activ_list (list): list of activating metabolites.
 
     Returns:
         grasp_pattern (str): the grasp pattern
@@ -72,11 +77,14 @@ def _generate_grasp_pattern(enz_states_list):
     prod_dic = {}
     subs_i = 0
     prod_i = 0
+    inhib_dic = dict((met, f'I{i+1}') for i, met in enumerate(inhib_list)) if inhib_list else dict()
+    activ_dic = dict((met, f'Z{i+1}') for i, met in enumerate(activ_list)) if activ_list else dict()
 
     rxn_i = 0
+    rxn_change = False
+
     n_transitions = 0
     after_transition = False
-    first_prod_release = ''
 
     grasp_pattern = ''
     rate_i = 1
@@ -86,18 +94,25 @@ def _generate_grasp_pattern(enz_states_list):
         # if there is more than one transition step and the enzyme state is 1 again it is a promiscuous enzyme,
         #  restart dictionaries so that no repeated ligand names are used.
         if n_transitions > 0 and enz_states_list[state_i][0] == 1:
+
             rxn_i += 1
+            rxn_change = True
             subs_dic = {}
             prod_dic = {}
 
         # get binding ligand name
         bind_ligand = ''
         if len(enz_states_list[state_i]) == 2:
-            if not enz_states_list[state_i][1] in subs_dic.keys():
-                subs_dic[enz_states_list[state_i][1]] = subs_list[subs_i]
-                subs_i += 1
+            if enz_states_list[state_i][1] in inhib_dic.keys():
+                bind_ligand = f'.*{inhib_dic[enz_states_list[state_i][1]]}'
+            elif enz_states_list[state_i][1] in activ_dic.keys():
+                bind_ligand = f'.*{activ_dic[enz_states_list[state_i][1]]}'
+            else:
+                if not enz_states_list[state_i][1] in subs_dic.keys():
+                    subs_dic[enz_states_list[state_i][1]] = subs_list[subs_i]
+                    subs_i += 1
 
-            bind_ligand = f'.*{subs_dic[enz_states_list[state_i][1]]}'
+                bind_ligand = f'.*{subs_dic[enz_states_list[state_i][1]]}'
 
         # write forward step
         grasp_pattern += f'{enz_states_list[state_i][0]} {enz_states_list[state_i + 1][0]} k{rate_i:02}{bind_ligand}\n'
@@ -110,14 +125,16 @@ def _generate_grasp_pattern(enz_states_list):
                 prod_dic[enz_states_list[state_i + 1][1]] = prod_list[prod_i]
                 prod_i += 1
 
-            release_ligand = first_prod_release if prod_dic[enz_states_list[state_i + 1][1]] == 'P' \
+            # by default P is named P1
+            release_ligand = f'.*P1' if prod_dic[enz_states_list[state_i + 1][1]] == 'P' \
                              else f'.*{prod_dic[enz_states_list[state_i + 1][1]]}'
 
             # if ligand unbinding is the first one after the transition step name it Pi
-            if after_transition:
+            if after_transition and rxn_change:
                 release_ligand = f'.*P{rxn_i+1}'
-                first_prod_release = f'.*P{rxn_i+1}'
                 after_transition = False
+                rxn_change = False
+
                 if rxn_i > 0:
                     prod_i -= 1
 
@@ -130,7 +147,8 @@ def _generate_grasp_pattern(enz_states_list):
         grasp_pattern += f'{enz_states_list[state_i + 1][0]} {enz_states_list[state_i][0]} k{rate_i:02}{release_ligand}\n'
         rate_i += 1
 
-    if n_transitions == 1:
+    # if there is only one reaction all P1 products should be P
+    if rxn_i == 0:
         grasp_pattern = grasp_pattern.replace('P1', 'P')
 
     # remove last new line
@@ -139,13 +157,15 @@ def _generate_grasp_pattern(enz_states_list):
     return grasp_pattern
 
 
-def convert_er_mech_to_grasp_pattern(file_in: str, file_out: str, inhib_list: list=None , activ_list: list=None):
+def convert_er_mech_to_grasp_pattern(file_in: str, file_out: str, inhib_list: list = None , activ_list: list = None):
     """
     Given an input file with a mechanism in the form of elementary reactions, converts it to a GRASP pattern file.
 
     Args:
         file_in (str): path to the input file with elementary reactions mechanism.
         file_out: path to the output file with GRASP pattern.
+        inhib_list (list): list of inhibiting metabolites.
+        activ_list (list): list of activating metabolites.
 
     Returns:
         None
@@ -155,7 +175,7 @@ def convert_er_mech_to_grasp_pattern(file_in: str, file_out: str, inhib_list: li
         er_mech = f_in.read()
 
     enz_states_list = _get_enz_states(er_mech)
-    grasp_pattern = _generate_grasp_pattern(enz_states_list)
+    grasp_pattern = _generate_grasp_pattern(enz_states_list, inhib_list, activ_list)
 
     with open(file_out, 'w+') as f_out:
         f_out.write(grasp_pattern)
--- set_up_grasp_models/tests/test_convert_mechanism.py
+++ set_up_grasp_models/tests/test_convert_mechanism.py
@@ -4,7 +4,7 @@ import unittest
 from set_up_grasp_models.set_up_grasp_models.convert_mechanisms import convert_er_mech_to_grasp_pattern
 
 
-class TestIO(unittest.TestCase):
+class TestConvertMechanism(unittest.TestCase):
 
     def setUp(self):
         self.test_folder = os.path.join('test_files', 'test_set_up_models', 'convert_mechanism')
@@ -25,14 +25,14 @@ class TestIO(unittest.TestCase):
 
         self.assertEqual(true_res, res)
 
-    # TODO add support for inhibitors/activators
     def test_convert_er_mech_to_grasp_pattern_AANATCompInhibIndep(self):
         true_res_file = os.path.join(self.test_folder, 'true_res_AANATCompInhibIndep.txt')
 
         file_in = os.path.join(self.test_folder, 'AANATCompInhibIndep_mech_er.txt')
         file_out = os.path.join(self.test_folder, 'AANATCompInhibIndep_mech_grasp.txt')
+        inhib_list = ['mltn_c']
 
-        convert_er_mech_to_grasp_pattern(file_in, file_out)
+        convert_er_mech_to_grasp_pattern(file_in, file_out, inhib_list=inhib_list)
 
         with open(true_res_file, 'r') as f_in:
             true_res = f_in.read()
@@ -58,11 +58,106 @@ class TestIO(unittest.TestCase):
 
         self.assertEqual(true_res, res)
 
+    def test_convert_er_mech_to_grasp_pattern_orderedBiBiActiv(self):
+        true_res_file = os.path.join(self.test_folder, 'true_res_orderedBiBiActiv.txt')
+
+        file_in = os.path.join(self.test_folder, 'orderedBiBiActiv_mech_er.txt')
+        file_out = os.path.join(self.test_folder, 'orderedBiBiActiv_mech_grasp.txt')
+        activ_list = ['pyr_c']
+
+        convert_er_mech_to_grasp_pattern(file_in, file_out, activ_list=activ_list)
+
+        with open(true_res_file, 'r') as f_in:
+            true_res = f_in.read()
+
+        with open(file_out, 'r') as f_in:
+            res = f_in.read()
+
+        self.assertEqual(true_res, res)
+
+    def test_convert_er_mech_to_grasp_pattern_orderedBiBiActivTwoTracks(self):
+        true_res_file = os.path.join(self.test_folder, 'true_res_orderedBiBiActivTwoTracks.txt')
+
+        file_in = os.path.join(self.test_folder, 'orderedBiBiActivTwoTracks_mech_er.txt')
+        file_out = os.path.join(self.test_folder, 'orderedBiBiActivTwoTracks_mech_grasp.txt')
+        activ_list = ['pyr_c']
+
+        convert_er_mech_to_grasp_pattern(file_in, file_out, activ_list=activ_list)
+
+        with open(true_res_file, 'r') as f_in:
+            true_res = f_in.read()
+
+        with open(file_out, 'r') as f_in:
+            res = f_in.read()
+
+        self.assertEqual(true_res, res)
+
     def test_convert_er_mech_to_grasp_pattern_orderedBiBiCompInhib(self):
-        return 0
+        true_res_file = os.path.join(self.test_folder, 'true_res_orderedBiBiCompInhib.txt')
+
+        file_in = os.path.join(self.test_folder, 'orderedBiBiCompInhib_mech_er.txt')
+        file_out = os.path.join(self.test_folder, 'orderedBiBiCompInhib_mech_grasp.txt')
+        inhib_list = ['pyr_c']
+
+        convert_er_mech_to_grasp_pattern(file_in, file_out, inhib_list=inhib_list)
+
+        with open(true_res_file, 'r') as f_in:
+            true_res = f_in.read()
+
+        with open(file_out, 'r') as f_in:
+            res = f_in.read()
+
+        self.assertEqual(true_res, res)
+
+    def test_convert_er_mech_to_grasp_pattern_orderedBiBiUncompInhib(self):
+        true_res_file = os.path.join(self.test_folder, 'true_res_orderedBiBiUncompInhib.txt')
+
+        file_in = os.path.join(self.test_folder, 'orderedBiBiUncompInhib_mech_er.txt')
+        file_out = os.path.join(self.test_folder, 'orderedBiBiUncompInhib_mech_grasp.txt')
+        inhib_list = ['pyr_c']
+
+        convert_er_mech_to_grasp_pattern(file_in, file_out, inhib_list=inhib_list)
+
+        with open(true_res_file, 'r') as f_in:
+            true_res = f_in.read()
+
+        with open(file_out, 'r') as f_in:
+            res = f_in.read()
+
+        self.assertEqual(true_res, res)
+
+    def test_convert_er_mech_to_grasp_pattern_orderedBiBiMixedInhib(self):
+        true_res_file = os.path.join(self.test_folder, 'true_res_orderedBiBiMixedInhib.txt')
+
+        file_in = os.path.join(self.test_folder, 'orderedBiBiMixedInhib_mech_er.txt')
+        file_out = os.path.join(self.test_folder, 'orderedBiBiMixedInhib_mech_grasp.txt')
+        inhib_list = ['pyr_c']
+
+        convert_er_mech_to_grasp_pattern(file_in, file_out, inhib_list=inhib_list)
+
+        with open(true_res_file, 'r') as f_in:
+            true_res = f_in.read()
+
+        with open(file_out, 'r') as f_in:
+            res = f_in.read()
+
+        self.assertEqual(true_res, res)
 
     def test_convert_er_mech_to_grasp_pattern_orderedBiBiRapidEquilibrium(self):
-        return 0
+        true_res_file = os.path.join(self.test_folder, 'true_res_orderedBiBiRapidEquilibrium.txt')
+
+        file_in = os.path.join(self.test_folder, 'orderedBiBiRapidEquilibrium_mech_er.txt')
+        file_out = os.path.join(self.test_folder, 'orderedBiBiRapidEquilibrium_mech_grasp.txt')
+
+        convert_er_mech_to_grasp_pattern(file_in, file_out)
+
+        with open(true_res_file, 'r') as f_in:
+            true_res = f_in.read()
+
+        with open(file_out, 'r') as f_in:
+            res = f_in.read()
+
+        self.assertEqual(true_res, res)
 
     def test_convert_er_mech_to_grasp_pattern_orderedUniBi(self):
         true_res_file = os.path.join(self.test_folder, 'true_res_orderedUniBi.txt')
@@ -97,14 +192,66 @@ class TestIO(unittest.TestCase):
         self.assertEqual(true_res, res)
 
     def test_convert_er_mech_to_grasp_pattern_randomBiBi(self):
-        return 0
+        true_res_file = os.path.join(self.test_folder, 'true_res_randomBiBi.txt')
+
+        file_in = os.path.join(self.test_folder, 'randomBiBi_mech_er.txt')
+        file_out = os.path.join(self.test_folder, 'randomBiBi_mech_grasp.txt')
+
+        convert_er_mech_to_grasp_pattern(file_in, file_out)
+
+        with open(true_res_file, 'r') as f_in:
+            true_res = f_in.read()
+
+        with open(file_out, 'r') as f_in:
+            res = f_in.read()
+
+        self.assertEqual(true_res, res)
 
     def test_convert_er_mech_to_grasp_pattern_randomBiBiCompInhib(self):
-        return 0
+        true_res_file = os.path.join(self.test_folder, 'true_res_randomBiBiCompInhib.txt')
+
+        file_in = os.path.join(self.test_folder, 'randomBiBiCompInhib_mech_er.txt')
+        file_out = os.path.join(self.test_folder, 'randomBiBiCompInhib_mech_grasp.txt')
+        inhib_list = ['pyr_c']
+
+        convert_er_mech_to_grasp_pattern(file_in, file_out, inhib_list=inhib_list)
+
+        with open(true_res_file, 'r') as f_in:
+            true_res = f_in.read()
+
+        with open(file_out, 'r') as f_in:
+            res = f_in.read()
+
+        self.assertEqual(true_res, res)
 
     def test_convert_er_mech_to_grasp_pattern_randomUniBi(self):
-        return 0
+        true_res_file = os.path.join(self.test_folder, 'true_res_randomUniBi.txt')
+
+        file_in = os.path.join(self.test_folder, 'randomUniBi_mech_er.txt')
+        file_out = os.path.join(self.test_folder, 'randomUniBi_mech_grasp.txt')
+
+        convert_er_mech_to_grasp_pattern(file_in, file_out)
+
+        with open(true_res_file, 'r') as f_in:
+            true_res = f_in.read()
+
+        with open(file_out, 'r') as f_in:
+            res = f_in.read()
+
+        self.assertEqual(true_res, res)
 
     def test_convert_er_mech_to_grasp_pattern_uniUniPromiscuous(self):
-        return 0
+        true_res_file = os.path.join(self.test_folder, 'true_res_uniUniPromiscuous.txt')
 
+        file_in = os.path.join(self.test_folder, 'uniUniPromiscuous_mech_er.txt')
+        file_out = os.path.join(self.test_folder, 'uniUniPromiscuous_mech_grasp.txt')
+
+        convert_er_mech_to_grasp_pattern(file_in, file_out)
+
+        with open(true_res_file, 'r') as f_in:
+            true_res = f_in.read()
+
+        with open(file_out, 'r') as f_in:
+            res = f_in.read()
+
+        self.assertEqual(true_res, res)
