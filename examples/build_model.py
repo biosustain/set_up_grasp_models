702dc74eeb06f21aedabc7a92a28f1cbddb12507
martamatos <marta.ra.matos@gmail.com>
2019-06-25 13:02:03 +0200

    Chore: re-organizing the examples folder

--- examples/build_model.py
+++ examples/build_model.py
@@ -6,34 +6,34 @@ import os
 
 def generate_base_model():
 
-    file_in_stoic = os.path.join('build_model', 'model_with_PPP_plaintext.txt')
+    file_in_stoic = os.path.join('models', 'glycolysis_example.txt')
     general_file = os.path.join('..', 'base_files', 'GRASP_general.xlsx')
     model_name = 'model_v1'
-    file_out = os.path.join('build_model', model_name + '.xlsx')
+    file_out = os.path.join('models', model_name + '.xlsx')
 
     set_up_model(model_name, file_in_stoic, general_file, file_out)
 
 
 def check_fluxes():
 
-    file_in = os.path.join('build_model', 'model_v1_manual3.xlsx')
+    file_in = os.path.join('models', 'model_v1_manual3.xlsx')
     data_dict = pd.read_excel(file_in, sheet_name=None)
     check_flux_balance(data_dict)
 
 
 def generate_model_from_base():
 
-    file_in_stoic = os.path.join('build_model', 'model_with_PPP_plaintext.txt')
-    base_excel_file = os.path.join('build_model', 'model_v1_manual2_EX.xlsx')
+    file_in_stoic = os.path.join('models', 'glycolysis_example.txt')
+    base_excel_file = os.path.join('models', 'model_v1_manual2_EX.xlsx')
     model_name = 'model_v2'
-    file_in_mets_conc = os.path.join('build_model', 'met_concs.xlsx')
+    file_in_mets_conc = os.path.join('models', 'met_concs.xlsx')
 
-    file_out = os.path.join('build_model', model_name + '.xlsx')
+    file_out = os.path.join('models', model_name + '.xlsx')
 
     set_up_model(model_name, file_in_stoic, base_excel_file, file_out, use_equilibrator=True,
                  file_in_mets_conc=file_in_mets_conc)
 
 
-#generate_base_model()
-#check_fluxes()
+generate_base_model()
+check_fluxes()
 generate_model_from_base()
